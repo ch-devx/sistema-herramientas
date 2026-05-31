abf2feb61baf40085e64e2dda261f46e57169e11
Carlos Heredia <carlosheredia2709@gmail.com>
2026-05-31 17:28:08 -0400

    feat(header.php): add 'Reportes' link to sidebar navigation

--- includes/header.php
+++ includes/header.php
@@ -38,6 +38,10 @@
             <svg viewBox="0 0 24 24"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"/></svg>
             <span class="sb-link-txt">Historial</span>
         </a>
+        <a href="/sistema-herramientas/modules/reportes/reporte_inventario.php" class="sb-link <?= strpos($_SERVER['PHP_SELF'], '/reportes/') !== false ? 'active' : '' ?>">
+            <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>
+            <span class="sb-link-txt">Reportes</span>
+        </a>
         <a href="/sistema-herramientas/modules/categorias/index.php" class="sb-link <?= strpos($_SERVER['PHP_SELF'], '/categorias/') !== false ? 'active' : '' ?>">
             <svg viewBox="0 0 24 24"><path d="M4 6h16M4 12h16M4 18h7"/></svg>
             <span class="sb-link-txt">Categorías</span>
